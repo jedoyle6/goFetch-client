b025caa777637e8de9631eac8ecefa7c856a7aa1
Jess Doyle <jessica.e.doyle@gmail.com>
2019-09-18 10:50:59 -0400

    added Card smoke test

--- src/Tests/Card.test.js
+++ src/Tests/Card.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import Card from '../Components/Card';
+import GameContext from '../GameContext';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,  div);
+    <GameContext.Provider value={{cardsLocked: false}}>
+      <Card id={'s01'}/>
+    </GameContext.Provider>
+  ,  div);
   ReactDOM.unmountComponentAtNode(div);
 });
