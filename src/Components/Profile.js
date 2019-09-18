961defdb1b5d4a4a4b87d72eb21c5b20e4dd36e5
Jess Doyle <jessica.e.doyle@gmail.com>
2019-09-18 16:20:28 -0400

    fixed handling of edge cases in profile fetch

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -30,12 +30,25 @@ class Profile extends React.Component {
   }
 
   render() {
+    let total_points = this.context.total_points;
+    console.log(total_points)
+    // if (!total_points) {
+    //   total_points = 0;
+    // }
+
+    let ranking_text;
+    if (this.context.rank < 1 || !this.context.rank) {
+      ranking_text = "You are currently unranked. Play games and earn points on the leaderboard to gain rank!"
+    } else {
+      ranking_text = `You are currently ranked as player ${this.context.rank} out of ${this.context.totalPlayers}.`
+    }
+
     return(
       <div className="profile-page">
         <h1 className="profile-text">{this.context.user_name}</h1>
         <img src={getAvatarByTeamId(this.context.team_id)} alt="Your player avatar" className="avatar"></img>
-        <h2 className="profile-text">Total Points: {this.context.total_points}</h2>
-        <h3 className="profile-text">You are currently ranked {this.context.rank} out of {this.context.totalPlayers}.</h3>
+        <h2 className="profile-text">Total Points: {total_points}</h2>
+        <h3 className="profile-text">{ranking_text}</h3>
       </div>
     );
   }
