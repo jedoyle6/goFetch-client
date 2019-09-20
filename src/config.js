6c282647540031c6e87375cf6f1022d6dbf284a7
Jess Doyle <jessica.e.doyle@gmail.com>
2019-09-20 11:28:21 -0400

    updated API endpoint for deployed database

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://arcane-everglades-91457.herokuapp.com/',
+    API_ENDPOINT: 'https://arcane-everglades-91457.herokuapp.com',
     TOKEN_KEY: 'secret-auth-token',
   }
