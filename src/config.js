7dc051322a4a1f91dfc5a59f685dc22625fcd213
Jess Doyle <jessica.e.doyle@gmail.com>
2019-11-12 13:33:41 -0500

    modify config for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    // API_ENDPOINT: 'https://arcane-everglades-91457.herokuapp.com',
-    API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: 'https://arcane-everglades-91457.herokuapp.com',
+    // API_ENDPOINT: 'http://localhost:8000',
     TOKEN_KEY: 'secret-auth-token',
   }
